cdcf13c552b9a3eee1b884b0934ccde17002a25b
Anh Kiệt <igg.anhkiet1@gmail.com>
2023-03-09 13:59:39 +0700

    init

---
+++ config.js
@@ -0,0 +1,15 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+    lines: [
+        "Gửi đến: Em bé đáng yêu",
+        "Hoa nở các mùa，Trăm hoa chứng thực",
+        "Thệ qua bao tuế nguyệt，Như phó thác cho khói bụi",
+        "Nụ cười của bạn，Làm mình vĩnh sinh quyến luyến",
+        "Tương tư thật chân tình，Nhất quyết không buông",
+        "Thân ái đích bảo bối，Bạn đích thị là duy nhất",
+    ],
+    date: "2017-07-20T20:15:00",
+    // 双方的名字
+    names: ["笨笨熊", "黑猪猪"]
+}
